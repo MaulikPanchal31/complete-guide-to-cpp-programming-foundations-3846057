55ea83eff97d9f9511c2250dd8fdf3efd2731d48
MaulikPanchal31 <mak.panchal3113@gmail.com>
2024-12-09 04:36:20 +0000

    01_02b -> 01_02m

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,18 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+/* > Below inclusion is optional depending on Compiler
+   > Chain of includes: iostream => istream => ostream => ios => xlocknum => cstdio
+   > The cstdio is C++ wrapper of stdio.h */
+#include <cstdio>
+
+// This is my Hello World program in C++
+// It starts with function 'main'
+int main(int argc, char* argv[]){ 
+    printf("Hello C World!\n\n");
+    std::cout << "I am from C++ World!" << std::endl;
+    std::cout << std::endl << std::endl;
+    return 0;
+}
